95da19cc573e9b6d4ee655f428cb9759271270f3
bytesmith <bytesmith@malloc.site>
2020-07-05 19:18:35 -0400

    added DefaultPathMap to store os dependant default paths

---
+++ pkg/DefaultPathMap.go
@@ -0,0 +1,17 @@
+package kooky
+
+// DefaultPathMap stores an os dependant file path
+type DefaultPathMap struct {
+	pathMap map[string]string
+}
+
+// Add adds a path to the map for a specific os
+func (c *DefaultPathMap) Add(operatingSystem string, path string) {
+	c.pathMap[operatingSystem] = path
+}
+
+// Get returns the path for a specific os
+func (c *DefaultPathMap) Get(operatingSystem string) (string, bool) {
+	val, found := c.pathMap[operatingSystem]
+	return val, found
+}
